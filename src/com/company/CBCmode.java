5ba06725f2fef6a3f951cbb215383f1d4eb5160f
stavlut <stavlut@gmail.com>
2017-04-15 21:39:28 +0300

    Merge branch 'master' of https://github.com/Simo-H/EDA
    # Conflicts:
    #	src/com/company/Main.java

---
+++ src/com/company/CBCmode.java
@@ -0,0 +1,48 @@
+package com.company;
+
+/**
+ * Handling the entire process of encryption and decoding by cbc mode
+ */
+public class CBCmode {
+
+    private int BlockSize;
+    private String[] textDivided;
+    private String Text;
+
+    public CBCmode(int blockSize,String text)
+    {
+        Text=text;
+        BlockSize=blockSize;
+        if ((text.length()% blockSize)== 0)
+        {
+            textDivided= new String[text.length()/ blockSize];
+        }
+        else
+        {
+            textDivided= new String[(text.length()/ blockSize)+1];
+        }
+        textDivided =new String[BlockSize];
+
+    }
+    public void Divided()
+    {
+        int j=0;
+        int mod=Text.length()% BlockSize ;
+        int div=Text.length()/ BlockSize;
+        for (int i=0; i<=div;i++)
+        {
+            for (int f=0; f<=10;i++)
+            {
+                textDivided[j] = textDivided[j]+Text.charAt(f);
+
+            }
+            j++;
+        }
+        for (int i=0; i<=mod;i++)
+        {
+            textDivided[j] = textDivided[j]+Text.charAt(i);
+        }
+        
+
+    }
+}
