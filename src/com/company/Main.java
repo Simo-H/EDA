658c498ad3ec84ce20d9a9820c5ed4475f30f593
stavlut <stavlut@gmail.com>
2017-04-20 19:32:17 +0300

    Merge branch 'master' of https://github.com/Simo-H/EDA
    # Conflicts:
    #	src/com/company/Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,29 +5,28 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         // write your code here
         ReadWrite RW = new ReadWrite();
-        String path = "C:\\Users\\Simo\\Desktop\\plainMsg_example.txt";
+        String path = "C:\\Users\\Stav\\Desktop\\cipherMsg_example.txt";
         String alltext = RW.ReadText(path);
 
         HashMap<Character ,Character> KeyMap=new HashMap<Character, Character>();
 
-        KeyMap=RW.ReadKey("C:\\Users\\Simo\\Desktop\\key_example.txt");
+        KeyMap=RW.ReadKey("C:\\Users\\Stav\\Desktop\\key_example.txt");
 
-        String pathForIV = "C:\\Users\\Simo\\Desktop\\IV_example.txt";
+        String pathForIV = "C:\\Users\\Stav\\Desktop\\IV_example.txt";
         String IV = RW.ReadText(pathForIV);
+        SubstitutionCipherED Encryption =new SubstitutionCipherED(KeyMap);
+       CBCmode CBC= new CBCmode(10, Encryption);
+       RW.WriteText("C:\\Users\\Stav\\Desktop\\test.txt",CBC.CBCDecryption(IV,alltext));
 
-        SubstitutionCipherAttack sa = new SubstitutionCipherAttack();
-        ArrayList<HashMap<Character,Character>> a = sa.findAllPossibleKeys(8);
-        ConcurrentHashMap<String,String> b = new ConcurrentHashMap<String,String>();
-        Set r = ConcurrentHashMap.newKeySet();
-        String test = RW.ReadText("C:\\Users\\Simo\\Desktop\\words.txt");
-        String[] test2 = test.split("\n");
-        for(String x:test2)
-        {
+       // ArrayList<HashMap<Character,Character>> a = sa.findAllPossibleKeys(8);
+        //ConcurrentHashMap<String,String> b = new ConcurrentHashMap<String,String>();
+        //Set r = ConcurrentHashMap.newKeySet();
+       // String test = RW.ReadText("C:\\Users\\Simo\\Desktop\\words.txt");
 
-        }
 
     }
 }
