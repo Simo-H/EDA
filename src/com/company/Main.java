fc24ecba866940dada9863a61f45056b4355113c
Simo <a>
2017-04-22 19:08:29 +0300

    Merge branch 'master' of https://github.com/Simo-H/EDA
    # Conflicts:
    #	src/com/company/CBCmode.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -15,26 +15,27 @@ public class Main {
     {
         // Decryption
         ReadWrite RW = new ReadWrite();
-        CBCmode CBC= new CBCmode(10);
-        byte[] alltext=  RW.ReadTextbyte("C:\\Users\\Simo\\Desktop\\Security Tests\\examples_ascii\\PartB\\cipher.txt");
+        //CBCmode CBC= new CBCmode(10);
+/*        byte[] alltext=  RW.ReadTextbyte("C:\\Users\\Simo\\Desktop\\Security Tests\\examples_ascii\\PartB\\cipher.txt");
         HashMap<Character ,Character> KeyMap=RW.ReadKey("C:\\Users\\Simo\\Desktop\\Security Tests\\examples_ascii\\PartB\\key_short.txt");
         byte[] IV = RW.ReadTextbyte("C:\\Users\\Simo\\Desktop\\Security Tests\\examples_ascii\\PartB\\IV_short.txt");
-        SubstitutionCipherED Encryption =new SubstitutionCipherED(KeyMap);
+        SubstitutionCipherED Encryption =new SubstitutionCipherED(KeyMap);*/
         //String a =CBC.CBCDecryption(IV,alltext,Encryption);
         //RW.WriteText("C:\\Users\\Simo\\Desktop\\test.txt",a);
 
         //Cipher text only attack
-        SubstitutionCipherAttack sbattack=new SubstitutionCipherAttack(CBC);
-        HashMap<Character ,Character> ans = sbattack.CipherTextOnlyAttack(alltext,IV,0.1, 8,0.01);
+/*        SubstitutionCipherAttack sbattack=new SubstitutionCipherAttack(CBC);
+        HashMap<Character ,Character> ans = sbattack.CipherTextOnlyAttack(alltext,IV,0.1, 8,0.01);*/
 
         //Known plainText attack
-/*        String IV = RW.ReadText("C:\\Users\\Simo\\Desktop\\PartC\\IV_long.txt");
-        String Key = RW.ReadText("C:\\Users\\Simo\\Desktop\\PartC\\key_long.txt");
-        String cipher = RW.ReadText("C:\\Users\\Simo\\Desktop\\PartC\\unknown_cipher1.txt");
-        String KnownTextPlain = RW.ReadText("C:\\Users\\Simo\\Desktop\\PartC\\known_plain_long.txt");
-        String KnownTextCipher = RW.ReadText("C:\\Users\\Simo\\Desktop\\PartC\\known_cipher.txt");
+        byte[] IV = RW.ReadTextbyte("C:\\Users\\Simo\\Desktop\\PartC\\IV_long.txt");
+        //HashMap<Character ,Character> Key = RW.ReadText("C:\\Users\\Simo\\Desktop\\PartC\\key_long.txt");
+        byte[] cipher = RW.ReadTextbyte("C:\\Users\\Simo\\Desktop\\PartC\\unknown_cipher1.txt");
+        byte[] KnownTextPlain = RW.ReadTextbyte("C:\\Users\\Simo\\Desktop\\PartC\\known_plain_long.txt");
+        byte[] KnownTextCipher = RW.ReadTextbyte("C:\\Users\\Simo\\Desktop\\PartC\\known_cipher.txt");
         CBCmode CBC= new CBCmode(8128);
         SubstitutionCipherAttack sbattack=new SubstitutionCipherAttack(CBC);
-        HashMap<Character ,Character> partialKey = sbattack.GetKeyFromPlainAndCipher(KnownTextCipher,KnownTextPlain,IV);*/
+        HashMap<Character ,Character> partialKey = sbattack.GetKeyFromPlainAndCipher(KnownTextCipher,KnownTextPlain,IV);
+        RW.WriteKey(partialKey,"C:\\Users\\Simo\\Desktop\\key.txt");
     }
 }
